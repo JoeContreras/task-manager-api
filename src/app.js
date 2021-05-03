e16fe41181ff328cb126ae842827b00cafe331b3
JoeContreras <joe.contreras809@gmail.com>
2021-05-02 19:35:28 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 require("./db/mongoose-db");
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+app.use(cors());
 
 module.exports = app;
