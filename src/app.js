9e6e54a065abb7d51d451876a3df4ea35a1500c6
JoeContreras <joe.contreras888@gmail.com>
2021-05-02 20:15:00 -0500

    changed cors again

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 
 require("./db/mongoose-db");
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
-const cors = require("cors")
 const port = process.env.PORT;
-app.use(cors());
+
 
 app.listen(port, () => {
   console.log(`Sever running in  port: ${port}`);
